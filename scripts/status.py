9ae72ecf225bfd0a74e9b9b058dc5a5b24a8bb4c
Mahesh Dhulipalla <dhulipallamahesh02@gmail.com>
2019-01-19 16:07:54 +0530

    changes

--- scripts/status.py
+++ scripts/status.py
@@ -1,7 +1,7 @@
 import sys
 
 status = ""
-with open(raw_input("Plese enter input file name: ")) as f:
+with open("status.txt") as f:
     for line in f:
     	if "commit" in line:
     		status = "False"
@@ -9,6 +9,6 @@ with open(raw_input("Plese enter input file name: ")) as f:
     	if "insertion" in line:
     		status = "True"
     		break
-   	file=open(raw_input("Please enter Output file name:"),"w+")
+   	file=open("output.txt",w+")
     file.write(status)
     file.close()
